89ecacea485935fdd4ba4e13dc53d9b9c401406c
michaelwright235 <maiklpolkovnikov@gmail.com>
2024-02-17 22:13:33 +0300

    Init (WIP)

---
+++ src/error.rs
@@ -0,0 +1,34 @@
+use thiserror::Error;
+use crate::{ARCHIVER, ARCHIVER_VERSION};
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    PlistError(#[from] plist::Error),
+    #[error("Incorrect NSKeyedArchive format: {0}")]
+    IncorrectFormat(String),
+    #[error("Missing object #{0}")]
+    MissingObject(u64),
+    #[error("Missing key '{0}' for object '{1}'")]
+    MissingObjectKey(String, String),
+    #[error("Wrong object value type. Expected '{0}' for key '{1}'")]
+    WrongObjectValueType(String, String)
+}
+
+impl From<IncorrectFormatError> for Error {
+    fn from(value: IncorrectFormatError) -> Self {
+        Self::IncorrectFormat(value.to_string())
+    }
+}
+
+#[derive(Error, Debug)]
+pub(crate) enum IncorrectFormatError {
+    #[error("Expected '{0}' key to be a type of '{1}'")]
+    WrongValueType(&'static str, &'static str),
+    #[error("Missing '{0}' header key")]
+    MissingHeaderKey(&'static str),
+    #[error("Unsupported archiver. Only '{ARCHIVER}' is supported")]
+    UnsupportedArchiver,
+    #[error("Unsupported archiver version. Only '{ARCHIVER_VERSION}' is supported")]
+    UnsupportedArchiverVersion,
+}
---
+++ src/lib.rs
@@ -0,0 +1,375 @@
+mod error;
+
+use std::{borrow::BorrowMut, cell::{Cell, OnceCell, RefCell}, collections::HashMap, rc::{Rc, Weak}};
+
+use log::debug;
+use paste::paste;
+pub use plist;
+pub use error::*;
+use plist::{Dictionary, Integer, Value};
+
+pub(crate) const ARCHIVER: &str = "NSKeyedArchiver";
+pub(crate) const ARCHIVER_VERSION: u64 = 100000;
+
+const ARCHIVER_KEY_NAME: &str = "$archiver";
+const TOP_KEY_NAME: &str = "$top";
+const OBJECTS_KEY_NAME: &str = "$objects";
+const VERSION_KEY_NAME: &str = "$version";
+const NULL_OBJECT_REFERENCE_NAME: &str = "$null";
+
+macro_rules! get_key {
+    ($self:ident, $key:ident, $typ:literal) => {
+        paste! {
+            {
+                if !$self.contains_key($key) {
+                    return Err(Error::MissingObjectKey($self.class().to_string(), $key.to_string()))
+                }
+                let obj = $self.fields.get($key).unwrap().[<as_$typ>]();
+                if obj.is_none() {
+                    return Err(Error::WrongObjectValueType($typ.to_string(), $key.to_string()))
+                }
+                obj.unwrap()
+            }
+        }
+    };
+}
+
+// Possible values inside of $objects
+#[derive(Debug)]
+enum ArchiveValue {
+    String(String),
+    Integer(Integer),
+    F64(f64),
+    NullRef,
+    Classes(Vec<String>),
+    Object(Object),
+}
+
+#[derive(Debug)]
+struct ObjectList(Vec<ArchiveValue>);
+impl ObjectList {
+    pub(crate) fn set_list(&mut self, list: Vec<ArchiveValue>) {
+        self.0 = list;
+    }
+    pub fn list(&self) -> &[ArchiveValue] {
+        &self.0
+    }
+}
+
+pub struct NSKeyedUnarchiver {
+    objects: Rc<RefCell<ObjectList>>,
+    top: Dictionary,
+}
+
+impl NSKeyedUnarchiver {
+    pub fn new(plist: Value) -> Result<Self, Error> {
+        let Some(mut dict) = plist.into_dictionary() else {
+            return Err(IncorrectFormatError::WrongValueType("root", "Dictionary").into());
+        };
+
+        // Check $archiver key
+        let archiver_key = Self::get_header_key(&mut dict, ARCHIVER_KEY_NAME)?;
+        let Some(archiver_str) = archiver_key.as_string() else {
+            return Err(IncorrectFormatError::WrongValueType(ARCHIVER_KEY_NAME, "String").into());
+        };
+
+        if archiver_str != ARCHIVER {
+            return Err(IncorrectFormatError::UnsupportedArchiver.into());
+        }
+
+        // Check $version key
+        let version_key = Self::get_header_key(&mut dict, VERSION_KEY_NAME)?;
+        let Some(version_num) = version_key.as_unsigned_integer() else {
+            return Err(IncorrectFormatError::WrongValueType(VERSION_KEY_NAME, "Number").into());
+        };
+
+        if version_num != ARCHIVER_VERSION {
+            return Err(IncorrectFormatError::UnsupportedArchiverVersion.into());
+        }
+
+        // Check $top key
+        let top_key = Self::get_header_key(&mut dict, TOP_KEY_NAME)?;
+        let Some(top) = top_key.to_owned().into_dictionary() else {
+            return Err(IncorrectFormatError::WrongValueType(TOP_KEY_NAME, "Dictionary").into());
+        };
+
+        // Check $objects key
+        let objects_key = Self::get_header_key(&mut dict, OBJECTS_KEY_NAME)?;
+        let Some(raw_objects) = objects_key.into_array() else {
+            return Err(IncorrectFormatError::WrongValueType(OBJECTS_KEY_NAME, "Array").into());
+        };
+
+        let mut objects = Self::decode_objects(raw_objects);
+        let object_list: Rc<RefCell<ObjectList>> = Rc::from(RefCell::new(ObjectList{0: vec![]}));
+        for obj in &mut objects {
+            if let ArchiveValue::Object(i) = obj {
+                i.objects_tree = Some(Rc::clone(&object_list));
+            }
+        }
+        object_list.as_ref().borrow_mut().0 = objects;
+
+        Ok(Self {
+            objects: object_list,
+            top
+        })
+    }
+
+    pub fn test(&self) {
+        println!("{:#?}", self.objects.borrow().list());
+        let a = self.objects.borrow();
+        let list = a.list();
+        let b = [&list[1], &list[2]];
+    }
+
+    fn get_header_key(dict: &mut Dictionary, key: &'static str) -> Result<Value, Error> {
+        let Some(objects_value) = dict.remove(key) else {
+            return Err(IncorrectFormatError::MissingHeaderKey(key).into());
+        };
+        Ok(objects_value)
+    }
+
+    /// Reads a plist file and creates a new converter for it. It should have a
+    /// NSKeyedArchiver plist structure.
+    pub fn from_file<P: AsRef<std::path::Path>>(path: P) -> Result<Self, Error> {
+        let val: Value = plist::from_file(path)?;
+        Self::new(val)
+    }
+
+    /// Reads a plist from a byte slice and creates a new converter for it.
+    /// It should have a NSKeyedArchiver plist structure.
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, Error> {
+        let val: Value = plist::from_bytes(bytes)?;
+        Self::new(val)
+    }
+
+    /// Reads a plist from a seekable byte stream and creates a new converter
+    /// for it. It should have a NSKeyedArchiver plist structure.
+    pub fn from_reader<R: std::io::Read + std::io::Seek>(
+        reader: R,
+    ) -> Result<Self, Error> {
+        let val: Value = plist::from_reader(reader)?;
+        Self::new(val)
+    }
+
+    fn is_container(val: &Value) -> bool {
+        let Some(dict) = val.as_dictionary() else {
+            return false;
+        };
+        if let Some(cls) = dict.get("$class") {
+            cls.as_uid().is_some()
+        } else {
+            false
+        }
+    }
+}
+
+impl NSKeyedUnarchiver {
+    fn decode_objects(objects: Vec<Value>) -> Vec<ArchiveValue> {
+        let mut decoded_objects = Vec::with_capacity(objects.len());
+        for obj in objects {
+            debug!("{:?}", obj);
+            let decoded_obj =
+            if let Some(s) = obj.as_string() {
+                if s == NULL_OBJECT_REFERENCE_NAME {
+                    ArchiveValue::NullRef
+                } else {
+                    ArchiveValue::String(obj.into_string().unwrap())
+                }
+            } else if let Value::Integer(i) = obj {
+                ArchiveValue::Integer(i)
+            } else if let Some(f) = obj.as_real() {
+                ArchiveValue::F64(f)
+            } else if let Some(dict) = obj.as_dictionary() {
+                if Self::is_container(&obj) {
+                    debug!("Is object");
+                    ArchiveValue::Object(Object::from_dict(obj.into_dictionary().unwrap()))
+                }
+                else if dict.contains_key("$classes") {
+                    if let Some(classes_arr) = obj.into_dictionary().unwrap().remove("$classes").unwrap().into_array() {
+                        let mut classes = Vec::with_capacity(classes_arr.len());
+                        for class in classes_arr {
+                            if let Some(s) = class.into_string() {
+                                classes.push(s)
+                            } else {
+                                panic!("Incorrect Classes object")
+                            }
+                        }
+                        ArchiveValue::Classes(classes)
+                    } else {
+                        panic!("Incorrect Classes object")
+                    }
+                } else {
+                    panic!("Unexpected object type")
+                }
+            } else {
+                panic!("Unexpected object type")
+            };
+            decoded_objects.push(decoded_obj);
+        }
+        decoded_objects
+    }
+}
+
+#[derive(Debug)]
+enum ObjectValue {
+    String(String),
+    Integer(Integer),
+    F64(f64),
+    Boolean(bool),
+    Array(Vec<u64>), // vector of uids
+    ObjectRef(u64),
+    NullRef
+}
+
+pub struct Object {
+    classes_uid: u64,
+    fields: HashMap<String, ObjectValue>,
+    objects_tree: Option<Rc<RefCell<ObjectList>>>
+}
+
+impl Object {
+    pub fn decode_bool(&self, key: &str) -> Result<bool, Error> {
+        //Ok(get_key!(self, key, "boolean"))
+        todo!()
+    }
+
+    pub fn decode_data(&self, key: &str) -> Result<Vec<u8>, Error> {
+        //Ok(get_key!(self, key, "data").to_vec())
+        todo!()
+    }
+
+    pub fn decode_f64(&self, key: &str) -> Result<f64, Error> {
+        //Ok(get_key!(self, key, "real"))
+        todo!()
+    }
+
+    pub fn decode_i64(&self, key: &str) -> Result<i64, Error> {
+        //Ok(get_key!(self, key, "signed_integer"))
+        todo!()
+    }
+
+    pub fn decode_u64(&self, key: &str) -> Result<u64, Error> {
+        //Ok(get_key!(self, key, "unsigned_integer"))
+        todo!()
+    }
+
+    pub fn decode_string(&self, key: &str) -> Result<String, Error> {
+        // As far as I can tell all strings inside of objects are
+        // linked with UIDs
+        // let uid = get_key!(self, key, "uid").get();
+        // let Some(obj) = self.objects_tree.get(uid as usize) else {
+        //     return Err(Error::MissingObject(uid));
+        // };
+        // if let Some(s) = obj.as_string() {
+        //     return Ok(s.to_string())
+        // } else {
+        //     return Err(Error::WrongObjectValueType("string".to_string(), key.to_string()))
+        // }
+        todo!()
+    }
+
+    fn is_container(val: &Value) -> bool {
+        let Some(dict) = val.as_dictionary() else {
+            return false;
+        };
+        if let Some(cls) = dict.get("$class") {
+            cls.as_uid().is_some()
+        } else {
+            false
+        }
+    }
+
+    pub fn decode_object(&self, key: &str) -> Result<(), Error> { // -> Result<&Object, Error>
+        // let uid = get_key!(self, key, "uid").get();
+        // let Some(obj) = self.objects_tree.get(uid as usize) else {
+        //     return Err(Error::MissingObject(uid));
+        // };
+        // if !Self::is_container(obj) {
+        //     return Err(Error::WrongObjectValueType("dictionary".to_string(), key.to_string()));
+        // }
+        // return new object from dict
+        todo!()
+    }
+
+    pub fn contains_key(&self, key: &str) -> bool {
+        self.fields.contains_key(key)
+    }
+
+    pub fn classes(&self) -> &[String] {
+        //&self.classes
+        todo!()
+    }
+
+    pub fn class(&self) -> &str {
+        //&self.classes[0]
+        todo!()
+    }
+
+    pub(crate) fn from_dict(mut dict: Dictionary) -> Self {
+        let classes_uid = dict.remove("$class").unwrap().into_uid().unwrap().get(); // unwrapping is safe, we previously check it with is_container()
+        let mut fields = HashMap::with_capacity(dict.len());
+        for (key, obj) in dict {
+            let decoded_obj =
+            if let Some(s) = obj.as_string() {
+                if s == NULL_OBJECT_REFERENCE_NAME {
+                    ObjectValue::NullRef
+                } else {
+                    ObjectValue::String(obj.into_string().unwrap())
+                }
+            } else if let Value::Integer(i) = obj {
+                ObjectValue::Integer(i)
+            } else if let Some(f) = obj.as_real() {
+                ObjectValue::F64(f)
+            } else if let Some(b) = obj.as_boolean() {
+                ObjectValue::Boolean(b)
+            } else if let Some(arr) = obj.as_array() {
+                let mut arr_of_uids = Vec::with_capacity(arr.len());
+                for val in obj.into_array().unwrap() {
+                    if val.as_uid().is_none() {
+                        panic!("Array should countain only uids")
+                    } else {
+                        arr_of_uids.push(val.into_uid().unwrap().get());
+                    }
+                }
+                ObjectValue::Array(arr_of_uids)
+            } else if let Some(_) = obj.as_uid() {
+                ObjectValue::ObjectRef(obj.into_uid().unwrap().get())
+            } else {
+                panic!("Unexpected object value type: {:?}", obj)
+            };
+            fields.insert(key, decoded_obj);
+        }
+        Self {
+            classes_uid,
+            fields,
+            objects_tree: None
+        }
+    }
+}
+
+impl std::fmt::Debug for Object {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Object")
+            .field("classes_uid", &self.classes_uid)
+            .field("fields", &self.fields)
+            //.field("objects_tree", &self.objects_tree.is_some())
+            .finish()
+    }
+}
+
+pub trait Decodable {
+    fn decode(object: Object) -> Self;
+}
+
+#[cfg(test)]
+mod tests {
+    use simplelog::{SimpleLogger, LevelFilter, Config};
+    use crate::NSKeyedUnarchiver;
+
+    #[test]
+    fn test1() {
+        let _ = SimpleLogger::init(LevelFilter::Debug, Config::default());
+        let unarchiver = NSKeyedUnarchiver::from_file("./MainWindow.nib").unwrap();
+        unarchiver.test();
+    }
+}
