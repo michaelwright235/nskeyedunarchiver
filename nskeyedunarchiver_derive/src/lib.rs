44eb2a1588e85ad03267064fd7d8cb3bbb8b82d4
michaelwright235 <maiklpolkovnikov@gmail.com>
2025-03-01 16:47:44 +0300

    Implement Derive for enums

--- nskeyedunarchiver_derive/src/lib.rs
+++ nskeyedunarchiver_derive/src/lib.rs
@@ -77,14 +77,10 @@ impl TryFrom<&[syn::Attribute]> for MacroAttributes {
     }
 }
 
-fn decodable_impl(input: DeriveInput) -> Result<TokenStream> {
+fn decodable_struct(input: &DeriveInput) -> Result<TokenStream> {
     let syn::Data::Struct(cur_struct) = &input.data else {
-        return Err(Error::new(
-            input.span(),
-            "Only structs are supported",
-        ));
+        unreachable!()
     };
-
     let syn::Fields::Named(named_fields) = &cur_struct.fields else {
         return Err(Error::new(
             input.span(),
@@ -228,6 +224,138 @@ fn decodable_impl(input: DeriveInput) -> Result<TokenStream> {
     };
 
     Ok(TokenStream::from(expanded))
+}
+
+fn decodable_enum(input: &DeriveInput) -> Result<TokenStream> {
+    let syn::Data::Enum(cur_enum) = &input.data else {
+        unreachable!()
+    };
+    let enum_ident = &input.ident;
+    let mut enum_name = enum_ident.to_string();
+
+    let enum_attrs = MacroAttributes::try_from(input.attrs.as_slice())?;
+    if enum_attrs.bool_attrs.len() != 0 || enum_attrs.str_attrs.len() != 0 {
+        return Err(Error::new(
+            input.span(),
+            "Attributes for enums are not supported",
+        ));
+    }
+
+    let variants = &cur_enum.variants;
+
+    let mut variants_inits: Vec<proc_macro2::TokenStream> =
+    Vec::with_capacity(variants.len());
+    let mut object_types = Vec::with_capacity(variants.len());
+
+    for v in variants {
+        // hangle things like Vec<u8> (brackets like <u8>)
+        let field_attrs = MacroAttributes::try_from(v.attrs.as_slice())?;
+        if field_attrs.bool_attrs.contains(&"skip".to_string()) {
+            continue;
+        }
+        eprintln!("{v:?}");
+        if *&v.fields.len() != 1 {
+            return Err(Error::new(
+                input.span(),
+                "An enum variant can only have one field",
+            ));
+        }
+        let field = &v.fields.iter().next().unwrap();
+
+        if let syn::Type::Path(b) = &field.ty {
+            let last_segment = b.path.segments.last().unwrap();
+            let last_segment_ident = &last_segment.ident;
+            if let syn::PathArguments::AngleBracketed(args) = &last_segment.arguments {
+                let a = args.to_token_stream();
+                let inner = quote! {
+                    #last_segment_ident::#a
+                };
+                object_types.push(inner);
+                continue;
+            }
+        }
+        // regular types
+        object_types.push(field.ty.to_token_stream());
+    }
+
+    for v in variants {
+        let field_ident = &v.ident;
+        let field_type = &v.fields.iter().next().unwrap().ty;
+
+        let field_attrs = MacroAttributes::try_from(v.attrs.as_slice())?;
+
+        if field_attrs.str_attrs.len() != 0 {
+            return Err(Error::new(
+                input.span(),
+                "This attribute is not supported for enum variants",
+            ));
+        }
+        if field_attrs.bool_attrs.contains(&"skip".to_string()) {
+            continue;
+        }
+
+        // hangle things like Vec<u8> (brackets like <u8>)
+        if let syn::Type::Path(b) = field_type {
+            //eprintln!("{:?}", b);
+            let last_segment = b.path.segments.last().unwrap();
+            let last_segment_ident = &last_segment.ident;
+            if let syn::PathArguments::AngleBracketed(args) = &last_segment.arguments {
+                let a = args.to_token_stream();
+                let inner = quote! {
+                    if let Ok(v) = #last_segment_ident::#a::decode(value.clone(), types) {
+                        return Ok(Self::#field_ident(v));
+                    }
+                };
+                variants_inits.push(inner);
+                continue;
+            }
+        }
+
+        // regular types
+        let inner = quote! {
+            if let Ok(v) = #field_type::decode(value.clone(), types) {
+                return Ok(Self::#field_ident(v));
+            }
+        };
+        variants_inits.push(inner);
+    }
+
+    let expanded = quote! {
+        impl Decodable for #enum_ident {
+            fn is_type_of(classes: &[String]) -> bool
+            where
+                Self: Sized {
+                false
+            }
+
+            fn class(&self) -> &str {
+                ""
+            }
+
+            fn decode(value: nskeyedunarchiver::ValueRef, types: &[ObjectType]) -> Result<Self, DeError>
+            where
+                Self: Sized {
+                #(#variants_inits)*
+
+                Err(DeError::Message(format!(
+                    "Undecodable object for enum: {value:?}",
+                )))
+            }
+        }
+    };
+
+    Ok(TokenStream::from(expanded))
+}
+
+fn decodable_impl(input: DeriveInput) -> Result<TokenStream> {
+    match &input.data {
+        syn::Data::Struct(_) => decodable_struct(&input),
+        syn::Data::Enum(_) => decodable_enum(&input),
+        _ => Err(Error::new(
+            input.span(),
+            "Only structs and enums are supported",
+        ))
+    }
 }
 
 #[proc_macro_derive(Decodable, attributes(decodable))]
